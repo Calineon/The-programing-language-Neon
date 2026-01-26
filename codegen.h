95bd956efc806cf506b6a185b7ed441ea8693264
neon.light <a8985289889@gmail.com>
2026-01-26 18:47:11 +0600

    Implement program parsing in codegen.h
    Added a parser function to handle program parsing logic, including loading statements and program structure.

--- codegen.h
+++ codegen.h
@@ -1,9 +1,27 @@
-#ifndef CODEGEN_H
-#define CODEGEN_H
+AstNode Parser::parse_program() {
+    AstNode program{ "program", "" };
 
-#include "parser.h"
-#include <string>
 
-std::string generate_c(const AstNode& node);
+    while (current.type == token_type::KEYWORD && current.value == "Load") {
+        expect(token_type::KEYWORD, "Load");
+        token id = expect(token_type::IDENTIFIER);
+        expect(token_type::SEMICOLON);
+        program.children.emplace_back(AstNode{ "load", id.value });
+    }
 
-#endif
+    
+    expect(token_type::KEYWORD, "program");
+    token nameTok = expect(token_type::IDENTIFIER);
+    program.children.emplace_back(AstNode{ "name", nameTok.value });
+    expect(token_type::SEMICOLON);
+    expect(token_type::KEYWORD, "begin");
+
+    
+    while (!(current.type == token_type::KEYWORD && current.value == "end")) {
+        if (current.type == token_type::EOF_TOKEN) {
+            throw std::runtime_error("Unexpected end of file: expected 'end' at program end");
+        }
+        program.children.push_back(parse_statement());
+    }
+    expect(token
+
