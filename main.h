0b7262dfa3bf3a0461d88f670513b091b1af6630
neon.light <a8985289889@gmail.com>
2026-01-26 18:53:33 +0600

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef NEON_MAIN_H
+#define NEON_MAIN_H
 
+#include <string>
+#include <string_view>
 #include "lexer.h"
 #include "parser.h"
 
 int compile_neon(const std::string& filename);
 
-#endif
+
+int compile_neon(std::string_view filename);
+
+#endif // NEON_MAIN_H
